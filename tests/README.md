4afdb71117c7a357db942a0fa395ed3510f2360e
John Collier <jcollier@redhat.com>
2021-04-23 14:29:52 -0400

    Add GitHub action for validating devfile stacks
    Signed-off-by: John Collier <jcollier@redhat.com>

---
+++ tests/README.md
@@ -0,0 +1,25 @@
+# Devfile Registry Testing
+
+## Prerequisites
+
+- Minikube installed, and running
+  - `minikube start --memory 8gb` is a good starting point.
+  - The `none` driver **cannot** be used. Any other driver (`docker`, `hyperkit`, etc) should suffice.
+- The ingress minikube addon **must** be installed with the `minikube addons enable ingress` command
+- odo v2 or later.
+
+## Running the tests
+
+1) Ensure minikube is running and `minikube ip` reports a valid ip address
+
+2) From the root of this repository, run `tests/test_minikube.sh`. 
+  
+    - The test script will validate each devfile stack under `stacks/` with odo, verifying that the stack can be used to build a starter project and that the application is properly built and exposed. 
+       - The test script checks for an HTTP 200 status code to determine "properly exposed".
+    - Each devfile stack **must** have at least one starter project specified in the devfile.yaml
+
+
+## Limitations
+
+- If there are multiple starter projects, odo will only use the first starter project mentioned.
+- Only `odo create`,  `odo url create`, and `odo push` are tested right now. If your devfile stack exposes additional functionality (such as debug, via `odo debug`), we recommend either manually testing that functionality, or setting up your own test scripts in the stack's repository
---
+++ tests/test_minikube.sh
@@ -0,0 +1,141 @@
+#!/bin/sh
+
+set -x
+DEVFILES_DIR="$(pwd)/stacks/"
+FAILED_TESTS=""
+
+getURLs() {
+    urls=$($ODO_PATH url list | awk '{ print $3 }' | tail -n +3 | tr '\n' ' ')
+    echo "$urls"
+}
+
+# periodicaly check url till it returns expected HTTP status
+# exit after 10 tries
+waitForHTTPStatus() {
+   url=$1
+   statusCode=$2
+
+    for i in $(seq 1 10); do
+        echo "try: $i"
+        content=$(curl -i "$url")
+        echo "Checking if $url is returning HTTP $statusCode"
+        echo "$content" | grep -q -E "HTTP/[0-9.]+ $statusCode"
+        retVal=$?
+        if [ $retVal -ne 0 ]; then
+            echo "ERROR not HTTP $statusCode"
+            echo "$content"
+        else
+            echo "OK HTTP $statusCode"
+            return 0
+        fi
+        sleep 10
+    done
+    return 1
+}
+
+# run test on devfile
+# parameters:
+# - name of a component and project 
+# - path to devfile.yaml
+test() {
+    devfileName=$1
+    devfilePath=$2
+
+    # remember if there was en error
+    error=false
+
+    tmpDir=$(mktemp -d)
+    cd "$tmpDir" || return 1
+
+    $ODO_PATH project create "$devfileName" || error=true
+    if $error; then
+        echo "ERROR project create failed"
+        FAILED_TESTS="$FAILED_TESTS $devfileName"
+        return 1
+    fi
+    
+    $ODO_PATH create "$devfileName" --devfile "$devfilePath" --starter || error=true
+    if $error; then
+        echo "ERROR create failed"
+        $ODO_PATH project delete -f "$devfileName"
+        FAILED_TESTS="$FAILED_TESTS $devfileName"
+        return 1
+    fi
+
+    # ToDo: Clean up, I'm not happy about having specific checks for the stacks with multiple ports
+    # But since we're testing against minikube, we need to specifically create the URL/ingress before pushing
+    # And if there's multiple ports in the devfile, a port must be specified.
+    if [ "$devfileName" = "java-wildfly" ] || [ "$devfileName" = "java-wildfly-bootable-jar" ]; then
+        $ODO_PATH url create --host "$(minikube ip).nip.io" --port 8080 || error=true
+    else
+        $ODO_PATH url create --host "$(minikube ip).nip.io" || error=true
+    fi
+    if $error; then
+        echo "ERROR url create failed"
+        $ODO_PATH project delete -f "$devfileName"
+        FAILED_TESTS="$FAILED_TESTS $devfileName"
+        return 1
+    fi
+
+    $ODO_PATH push || error=true
+    if $error; then
+        echo "ERROR push failed"
+        $ODO_PATH project delete -f "$devfileName"
+        FAILED_TESTS="$FAILED_TESTS $devfileName"
+        return 1
+    fi
+
+    # check if application is responding
+    urls=$(getURLs)
+
+    for url in $urls; do
+        statusCode=200
+        
+        waitForHTTPStatus "$url" "$statusCode"
+        if [ $? -ne 0 ]; then
+            echo "ERROR unable to get working url"
+            $ODO_PATH project delete -f "$devfileName"
+            FAILED_TESTS="$FAILED_TESTS $devfileName"
+            error=true
+            return 1
+        fi
+    done
+
+    # kill -9 $CPID
+    $ODO_PATH delete -f -a || error=true
+    $ODO_PATH project delete -f "$devfileName"
+
+    if $error; then
+        echo "FAIL"
+        # record failed test
+        FAILED_TESTS="$FAILED_TESTS $devfileName"
+        return 1
+    fi
+
+    return 0
+}
+
+ODO_PATH=$1
+if [ -z $ODO_PATH ]; then
+  ODO_PATH=odo
+fi
+
+for devfile_dir in $(find $DEVFILES_DIR -maxdepth 1 -type d ! -path $DEVFILES_DIR); do
+    devfile_name="$(basename $devfile_dir)"
+    devfile_path=$devfile_dir/devfile.yaml
+    if [ $devfile_name != "java-wildfly-bootable-jar" ]; then
+      test "$devfile_name" "$devfile_path"
+    fi
+done
+
+# remember if there was an error so the script can exist with proper exit code at the end
+error=false
+
+# print out which tests failed
+if [ "$FAILED_TESTS" != "" ]; then
+    error=true
+    echo "FAILURE: FAILED TESTS: $FAILED_TESTS"
+    exit 1
+fi
+
+exit 0
