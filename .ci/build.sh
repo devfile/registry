a2635157037e48edea87b9e93838f275f61528a5
John Collier <jcollier@redhat.com>
2021-05-04 11:35:30 -0400

    Add Dockerfile and OpenShift CI script
    Signed-off-by: John Collier <jcollier@redhat.com>

--- .ci/build.sh
+++ .ci/build.sh
@@ -11,27 +11,6 @@
 # This script downloads the registry build tools and builds up this repository
 # This script runs on both the GitHub action CI and the CICD for the hosted registry
 
-# cleanup_and_exit removes the registry-support folder we cloned and exits with the exit code passed into it
-ciFolder="$(dirname "$0")"
 ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cleanup_and_exit() {
-  rm -rf $ABSOLUTE_PATH/registry-support
-  exit $1
-}
-
-cd $ciFolder
-rm -rf registry-support/
-
-# Clone the build tools
-git clone https://github.com/devfile/registry-support.git
-if [ $? -ne 0 ]; then
-  echo "Failed to clone build tools repo"
-  cleanup_and_exit 1
-fi
-cd registry-support && git checkout cd279527f87340d6b51ee366e70140ecfceef960 && cd ..
-
-# Run the build script
-./registry-support/build-tools/build.sh ../
-
-cleanup_and_exit 0
+docker build -t devfile-index -f $ABSOLUTE_PATH/Dockerfile $ABSOLUTE_PATH/..
---
+++ .ci/openshift_integration.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+
+#!/usr/bin/env bash
+# exit immediately when a command fails
+set -e
+# only exit with zero if all commands of the pipeline exit successfully
+set -o pipefail
+# error on unset variables
+set -u
+# print each command before executing it
+set -x
+
+# Disable telemtry for odo
+export ODO_DISABLE_TELEMETRY=true
+
+# Split the registry image and image tag from the REGISTRY_IMAGE env variable
+IMG="$(echo $REGISTRY_IMAGE | cut -d':' -f1)"
+TAG="$(echo $REGISTRY_IMAGE | cut -d':' -f2)"
+
+# Build odo
+git clone https://github.com/openshift/odo.git
+cd odo && make
+cd ..
+
+# Install the devfile registry
+git clone https://github.com/devfile/registry-support.git
+oc process -f registry-support/deploy/hosted-registry/devfile-registry.yaml -p DEVFILE_INDEX_IMAGE=$IMG -p DEVFILE_INDEX_IMAGE_TAG=$TAG -p REPLICAS=3 | \
+  oc apply -f -
+
+# Deploy the routes for the registry
+oc process -f registry-support/deploy/hosted-registry/route.yaml | oc apply -f -
+
+# Wait for the registry to become ready
+oc wait deploy/devfile-registry --for=condition=Available --timeout=600s
+
+# Get the route URL for the registry
+REGISTRY_HOSTNAME=$(oc get route devfile-registry -o jsonpath="{.spec.host}")
+
+echo $REGISTRY_HOSTNAME
+
+# Delete the default devfile registry and add the test one we just stood up
+$(realpath odo/odo) registry delete DefaultDevfileRegistry -f
+$(realpath odo/odo) registry add TestDevfileRegistry http://$REGISTRY_HOSTNAME
+
+# Run the devfile validation tests
+ENV=openshift REGISTRY=remote tests/test.sh $(realpath odo/odo)
