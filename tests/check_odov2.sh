d6c45af56a79de7d87c034b1eef5d0b1526f2053
Paul Schultz <pschultz@pobox.com>
2022-11-21 14:42:42 -0600

    fixed odov2 workflow
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

--- tests/check_odov2.sh
+++ tests/check_odov2.sh
@@ -5,4 +5,4 @@ set -x
 ginkgo run --procs 2 \
   --slow-spec-threshold 120s \
   --timeout 2h \
-  tests/odov2 -- -stacksPath "$(pwd)"/stacks -stackDirs python
+  tests/odov2 -- -stacksPath "$(pwd)"/stacks -stackDirs python -env $ENV -registry $REGISTRY
