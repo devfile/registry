d19bf5fa92e747cd93c752f7e7b5d65cf29a4349
thepetk <thepetk@gmail.com>
2023-04-24 15:25:34 +0100

    Fix issue with bash script
    Signed-off-by: thepetk <thepetk@gmail.com>

--- tests/validate_devfile_schemas.sh
+++ tests/validate_devfile_schemas.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-stackDirs=$("bash $(pwd)/tests/get_stacks.sh")
+stackDirs=$(bash "$(pwd)/tests/get_stacks.sh")
 
 ginkgo run --procs 2 \
   tests/validate_devfile_schemas -- -stacksPath "$(pwd)"/stacks -stackDirs "$stackDirs"
