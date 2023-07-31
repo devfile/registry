7c0d08c3780917e6905f526be5cb5c6b619d62bb
Michael Valdron <mvaldron@redhat.com>
2023-07-31 16:33:17 -0400

    validate_devfile_schemas script accepts relative or absolute paths
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- tests/validate_devfile_schemas.sh
+++ tests/validate_devfile_schemas.sh
@@ -2,8 +2,13 @@
 
 set -x
 
-stacksDir=${STACKS_DIR:-"$(pwd)/stacks"}
+stacksDir=${STACKS_DIR:-stacks}
 stackDirs=${STACKS:-"$(bash "$(pwd)/tests/get_stacks.sh")"}
+
+# Use pwd if relative path
+if [[ ! ${stacksDir} = /* ]]; then
+  stacksDir=$(pwd)/${stacksDir}
+fi
 
 ginkgo run --procs 2 \
   tests/validate_devfile_schemas -- -stacksPath ${stacksDir} -stackDirs "$stackDirs"
