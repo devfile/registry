efb23aabdff74dbd29e32b3ad00d95f42737e7e2
Michael Valdron <mvaldron@redhat.com>
2025-01-09 14:07:47 -0500

    Fix PR integration testing (#531)
    * tls enablement for check_odov2.sh and openshift_integration.sh
    
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>
    
    * replace odov2 with odov3 under integration testing
    
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>
    
    * fix missing ginkgo and test script argument steps
    
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>
    
    * correct ENABLE_TLS to ENABLE_TLS_VERIFY for odov2 test script
    
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>
    
    * remove ENABLE_TLS_VERIFY for odov3 test script run under integration testing script
    
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>
    
    * revert showing full odo path in test log
    
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>
    
    * openshift_integration: fix remaining file path references to use BASE_DIR instead of relative path
    
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>
    
    ---------
    
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- .ci/openshift_integration.sh
+++ .ci/openshift_integration.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
+
+# Copyright Red Hat
 #
-#   Copyright 2021-2022 Red Hat, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 #!/usr/bin/env bash
 # exit immediately when a command fails
@@ -24,11 +24,16 @@ set -u
 # print each command before executing it
 set -x
 
+BASE_DIR="$(realpath $(dirname ${BASH_SOURCE[0]})/..)"
+
 # Disable telemtry for odo
 export ODO_DISABLE_TELEMETRY=true
 
 # Set yq version
 YQ_VERSION=${YQ_VERSION:-v4.44.1}
+
+# Set odo version
+ODO_VERSION=${ODO_VERSION:-v3.16.1}
 
 # Split the registry image and image tag from the REGISTRY_IMAGE env variable
 IMG="$(echo $REGISTRY_IMAGE | cut -d':' -f1)"
@@ -42,15 +47,19 @@ curl -sL https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linu
 YQ_PATH=$(realpath yq)
 
 # Download odo
-curl -sL https://developers.redhat.com/content-gateway/rest/mirror/pub/openshift-v4/clients/odo/v2.5.1/odo-linux-amd64 -o odo && chmod +x odo
+curl -sL https://developers.redhat.com/content-gateway/rest/mirror/pub/openshift-v4/clients/odo/${ODO_VERSION}/odo-linux-amd64 -o odo && chmod +x odo
 export GLOBALODOCONFIG=$(pwd)/preferences.yaml
 
+# Download & Install Ginkgo
+GINKGO_VERSION="$(cd $BASE_DIR/tests/odov3 && go list -m -json all | ${YQ_PATH} 'select(.Path == "github.com/onsi/ginkgo/v2") | .Version' -Mr -p=json)"
+go install github.com/onsi/ginkgo/v2/ginkgo@${GINKGO_VERSION}
+
 # Install the devfile registry
-oc process -f .ci/deploy/devfile-registry.yaml -p DEVFILE_INDEX_IMAGE=$IMG -p IMAGE_TAG=$TAG -p REPLICAS=3 -p ANALYTICS_WRITE_KEY= | \
+oc process -f $BASE_DIR/.ci/deploy/devfile-registry.yaml -p DEVFILE_INDEX_IMAGE=$IMG -p IMAGE_TAG=$TAG -p REPLICAS=3 -p ANALYTICS_WRITE_KEY= | \
   oc apply -f -
 
 # Deploy the routes for the registry
-oc process -f .ci/deploy/route.yaml | oc apply -f -
+oc process -f $BASE_DIR/.ci/deploy/route.yaml | oc apply -f -
 
 # Wait for the registry to become ready
 oc wait deploy/devfile-registry --for=condition=Available --timeout=600s
@@ -65,8 +74,8 @@ REGISTRY_HOSTNAME=$(oc get route devfile-registry -o jsonpath="{.spec.host}")
 echo $REGISTRY_HOSTNAME
 
 # Delete the default devfile registry and add the test one we just stood up
-$(realpath odo) registry delete DefaultDevfileRegistry -f
-$(realpath odo) registry add TestDevfileRegistry http://$REGISTRY_HOSTNAME
+$(realpath odo) preference remove registry DefaultDevfileRegistry -f
+$(realpath odo) preference add registry TestDevfileRegistry http://$REGISTRY_HOSTNAME
 
 # Run the devfile validation tests
-ENV=openshift REGISTRY=remote tests/check_odov2.sh $(realpath odo) $YQ_PATH
+ENV=openshift REGISTRY=remote $BASE_DIR/tests/check_odov3.sh $(realpath odo)
--- tests/check_odov2.sh
+++ tests/check_odov2.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
+#
+# Copyright Red Hat
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 set -x
 DEVFILES_DIR="$(pwd)/stacks"
 FAILED_TESTS=()
+ENABLE_TLS_VERIFY=${ENABLE_TLS_VERIFY:-"true"}
 
 # The stacks to test as a string separated by spaces
 STACKS=$(bash "$(pwd)/tests/get_stacks.sh")
@@ -42,7 +58,16 @@ waitForHTTPStatus() {
 
   for i in $(seq 1 10); do
     echo "try: $i"
-    content=$(curl -i "$url")
+    if [[ $ENABLE_TLS_VERIFY == "true" ]]
+    then
+      content=$(curl -i "$url")
+    elif [[ $ENABLE_TLS_VERIFY == "false" ]]
+    then
+      content=$(curl -i --insecure "$url")
+    else
+      echo "ERROR: ENABLE_TLS must be either true or false"
+      return 1
+    fi
     echo "Checking if $url is returning HTTP $status_code"
     echo "$content" | grep -q -E "HTTP/[0-9.]+ $status_code"
     ret_val=$?
