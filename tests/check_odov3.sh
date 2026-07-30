661402a2cd95c9e1bc5bbfb6fc08b5a80e27d99f
Oleksii Kurinnyi <okurinny@redhat.com>
2026-07-30 18:04:00 +0300

    feat: add OpenClaw AI assistant stack (#695)
    * feat: add OpenClaw AI assistant stack
    
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>
    
    * chore: add CODEOWNERS entry for OpenClaw stack
    
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>
    
    * chore: skip OpenClaw stack in odo v3 test
    
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>
    
    * fix: increase token entropy and avoid killing PID 1 on restart
    
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>
    
    * Update stacks/openclaw/1.0.0/devfile.yaml
    
    Co-authored-by: Ilya Buziuk <ibuziuk@redhat.com>
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>
    
    ---------
    
    Signed-off-by: Oleksii Kurinnyi <okurinny@redhat.com>
    Co-authored-by: Ilya Buziuk <ibuziuk@redhat.com>

--- tests/check_odov3.sh
+++ tests/check_odov3.sh
@@ -74,6 +74,7 @@ ginkgo run --mod=readonly --procs 2 \
   --skip="stack: java-quarkus" \
   --skip="stack: ollama" \
   --skip="stack: hermes" \
+  --skip="stack: openclaw" \
   --slow-spec-threshold 120s \
   --timeout 3h \
   tests/odov3 -- -stacksPath "$stacksPath" -stackDirs "$stackDirs" ${args}
